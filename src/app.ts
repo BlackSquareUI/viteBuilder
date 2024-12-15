35e1a2f5f8bf493289022852bd5e3661fd1ac033
dyooreen <dyooreen@icloud.com>
2024-12-15 22:21:37 +0400

    .

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import { build, getBuildConfig } from "@blacksquareui/cssbuilder"
+import { createCSSBundle, getConfig } from "@blacksquareui/cssbuilder"
 
 function blackSquareUI() {
     return {
         name: 'vite-plugin-blacksquareui',
         async transform(_code: string, id: string) {
-            const { sourceDir, fileExtension, outputFile } = await getBuildConfig()
-            if (id.endsWith(fileExtension)) {
-                await build({ sourceDir, fileExtension, outputFile })
+            const { content, props } = await getConfig()
+
+            if (id.endsWith(content.fileExtension)) {
+                await createCSSBundle(content.sourceDir, content.fileExtension, props, content.outputFile)
             }
         },
     };
